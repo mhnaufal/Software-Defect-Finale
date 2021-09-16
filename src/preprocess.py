61787ce88d0e2c13082dd6a24f9b89c4fd9284e8
mhnaufal <aal.pirates16@gmail.com>
2021-09-16 19:52:24 +0700

    Complete preprocessing on preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,27 +1,40 @@
 """
-PREPROCESS
+--- PREPROCESS ---
 
 'preprocess.py' contains methods to preprocess current dataset (PROMISE dataset)
 into a proper data format that fits the machine learning model(s) and print out the reuslts.
 
-This file contains 2 methods with the following details:
+This file contains 3 methods with the following details:
     1) 'preprocess' method, which will return features and label from the extracted dataset
         after being preprocessed by handling the empty value and imbalanced
     
-    2) 'log' method, which will print out the result of 'preprocess' method along side with 
+    2) 'preprocess_log' method, which will print out the result of 'preprocess' method along side with 
         the timestamp and running machine info
         and write it on preprocess.txt inside reports/preprocess.txt folder
+    
+    3) 'print_bar_diagram', which will print out the pyplot bar diagram representing the balance of the dataset
 
-author: anonymouse
+Author: anonymouse
         Sep 2021
+-------------------
 """
 
 from datetime import datetime
-import imblearn.over_sampling as SMOTE
+from imblearn.over_sampling import (
+    RandomOverSampler,
+    SMOTE,
+    ADASYN,
+    SMOTEN,
+    SMOTENC,
+    KMeansSMOTE,
+)
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+import getpass
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import sklearn.model_selection as train_test_split
 import platform
 
 
@@ -41,61 +54,84 @@ def preprocess(file):
     X = pd.DataFrame(dataset.drop(["defects"], axis=1))
     y = pd.DataFrame(dataset["defects"])
 
-    # Count how many defects and not defects data
+    ### Count how many defects and not defects data
     defect = y[y["defects"] == True].count()
     not_defect = y[y["defects"] == False].count()
-
-    # Print the bar diagram
-    plt.bar(
-        ["True", "False"],
-        [len(y[y["defects"] == True].index), len(y[y["defects"] == False].index)],
-        color="royalblue",
-        edgecolor="green",
-        width=0.4,
-        alpha=0.8,
-    )
-    plt.grid(color="#95a5a6", linestyle="--", linewidth=2, axis="y", alpha=0.7)
-    plt.xlabel("Defect Status")
-    plt.ylabel("Number of files")
-    plt.title("Software Defect Prediction")
-    plt.savefig("reports/figures/preprocess.png", bbox_inches="tight")
-    plt.show()
-
-    # print(list(y))
+    print_bar_diagram(y, "imbalanced")
 
     """
     Handle missing value
-    ! There is no missing value here
+    NOTE: There is no missing value here
     """
-    # print(X.isnull().values.any())
+    # print(X.isnull().values.any())    # To check the missing value in the features column
 
     """
     Handle/encode categorical data
+    NOTE: There is no categorical data anymore here
     """
 
     """
     Handle noisy data
+    NOTE: There is no noisy data here (hopefully)
     """
 
     """
     Handle imbalance dataset
+    NOTE: We use some different techniques to handle the imbalance data
     """
-    
+    # RandomOverSampler #
+    ros = RandomOverSampler(random_state=3301)
+    X_ros, y_ros = ros.fit_resample(X.values, y.values)
+    print_bar_diagram(y_ros, "balanced")
+
+    # RandomUnderSampler #
+    # rus = RandomUnderSampler()
+
+    # TomekLinks #
+
+    # SMOTE #
+    # smote = SMOTE()
+
+    # ADASYN #
+    # adasyn = ADASYN()
+
+    # NearMiss #
+
+    # SMOTENC #
+    # smotenc = SMOTENC()
+
+    # SMOTEN #
+    # smoten = SMOTEN()
+
+    # KMeanSmote #
+    # k_mean_smote = KMeansSMOTE()
 
     """
     Split data train & data test
     """
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_ros, y_ros, test_size=0.3, random_state=3301
+    )
+
+    X_train, X_validation, y_train, y_validation = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=3301
+    )
 
     """
     Feature scaling
+    NOTE: No need for feature scaling for now
     """
 
-    # return {"X": X, "y": y, "dataset": dataset}
-    # return {"X": X, "y": y}
-    return {"defect": defect, "not": not_defect}
+    # return {"defect": defect, "not": not_defect}
+    return X_ros, y_ros, X_train, y_train, X_test, y_test, X_validation, y_validation
 
 
-def log():
+def preprocess_log():
+    """
+    preprocess_log() -> pyplot diagram
+        This method will return a 'running log' of the above 'preprocess' method
+    """
+
     with open("reports/preprocess.txt", "w") as preprocess_file:
         print("+----- PREPROCESSING LOGGER -----+", file=preprocess_file)
         print(
@@ -112,9 +148,70 @@ def log():
             + platform.uname().machine,
             file=preprocess_file,
         )
+        print("Author: ", getpass.getuser(), file=preprocess_file)
         print(file=preprocess_file)
         print(preprocess("datasets/processed/pc4.csv"), file=preprocess_file)
         print("+--------------------------------+", file=preprocess_file)
 
 
-log()
+def print_bar_diagram(label, type):
+    """
+    print_bar_diagram(label, type) -> pyplot diagram
+        This method take two parameters,
+        - the first one is label which is the data that will be described in the diagram
+        - the second one is the type of data:
+            'imbalanced' = the data is in format of a single column with rows represent the value
+            'balanced' = the data is in format a list with all of the rows value
+        and will return a bar diagram saved in reports/figures/ folder or Exception error if the plotting type is unknown
+    """
+
+    if type == "imbalanced":
+        plt.bar(
+            ["True", "False"],
+            [
+                len(label[label["defects"] == True].index),
+                len(label[label["defects"] == False].index),
+            ],
+            color="royalblue",
+            edgecolor="green",
+            width=0.4,
+            alpha=0.8,
+        )
+        plt.grid(color="#95a5a6", linestyle="--", linewidth=2, axis="y", alpha=0.7)
+        plt.xlabel("Defect Status")
+        plt.ylabel("Number of files")
+        plt.suptitle("Software Defect Prediction", fontsize=12, fontweight="bold")
+        plt.title(["Imbalanced data", datetime.now().strftime("%H:%M:%S %d-%m-%Y")])
+
+        # Uncomment the following line to save the figure
+        plt.savefig(
+            "reports/figures/preprocess_imbalanced.png", bbox_inches="tight"
+        )
+        # plt.show()
+
+    elif type == "balanced":
+        plt.bar(
+            ["True", "False"],
+            [label.tolist().count(True), label.tolist().count(False)],
+            color="lime",
+            edgecolor="green",
+            width=0.4,
+            alpha=0.8,
+        )
+        plt.grid(color="#95a5a6", linestyle="--", linewidth=2, axis="y", alpha=0.7)
+        plt.xlabel("Defect Status")
+        plt.ylabel("Number of files")
+        plt.suptitle("Software Defect Prediction", fontsize=12, fontweight="bold")
+        plt.title(["Balanced data", datetime.now().strftime("%H:%M:%S %d-%m-%Y")])
+
+        # Uncomment the following line to save the figure
+        plt.savefig(
+            "reports/figures/preprocess_balanced.png", bbox_inches="tight"
+        )
+        # plt.show()
+
+    else:
+        raise Exception("Unknown plotting type")
+
+
+preprocess_log()
